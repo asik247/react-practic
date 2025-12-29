55324b245f568c2bf01fdf5c17c809a4758b39b6
Asikur Rahaman <mdasikur5893@gmail.com>
2025-12-30 01:07:49 +0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ const App = () => {
       <section className="hero min-h-[70vh] bg-base-200">
         <div className="hero-content text-center">
           <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
+            <h1 className="text-5xl font-bold">Hello everyone</h1>
             <p className="py-6">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi.

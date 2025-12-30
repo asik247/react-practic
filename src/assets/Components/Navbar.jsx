160470144144df22c77e71ecb93c7f858a1fec8c
Asikur Rahaman <mdasikur5893@gmail.com>
2025-12-30 12:12:33 +0600

    some code added

--- src/assets/Components/Navbar.jsx
+++ src/assets/Components/Navbar.jsx
@@ -1,1 +1,11 @@
-rafce
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>This is Navbar here.</h1>
+        </div>
+    );
+};
+
+export default Navbar;
